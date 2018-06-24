9c715a6fa5763b43c5e5fb24c0f9fe4fe3e97f74
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-06-23 21:27:42 -0400

    set up the controller for story

---
+++ controllers/story.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const story = require("../db/connection").story;
+
+
+
+
+
+
+
+//stays at the bottom of the file:
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express()
+const storyController = require("./controllers/story");
+const characterController = require("./controllers/character");
+const homeController = require("./controllers/home");
+const methodOverride = require("method-override");
+const parser = require("body-parser");
 
+
+
+app.use("/character", characterController);
+app.use("/story", storyController);
+app.use("/", homeController);
 app.set("view engine", "ejs");
 
 app.listen(4000, () =>
