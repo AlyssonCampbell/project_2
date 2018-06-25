944ec80a75da05227da8f8d82c5f8d2b69647882
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-06-24 20:18:49 -0400

    show for story functional

--- controllers/story.js
+++ controllers/story.js
@@ -5,15 +5,23 @@ const Story = connection.models.story;
 
 //set up for GET handler:
 router.get("/", (req, res) => {
-    Story.findAll().then(story => {
-        res.render("story", {
-            story
-        });
-    }).catch(err => console.error(err));
+    Story.findAll()
+        .then(story => {
+            res.render("story", {
+                story
+            });
+        })
+        .catch(err => console.error(err));
 });
 
-
-
+//show route for stories
+router.get("/:id", (req, res) => {
+    Story.findById(req.params.id).then(story =>
+        res.render("showstory", {
+            story
+        })
+    );
+});
 
 //stays at the bottom of the file:
 module.exports = router;
